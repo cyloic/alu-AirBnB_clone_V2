6aa042f4eb224d992a7677ea2a2e03cbbcbf1406
cyloic <145006763+cyloic@users.noreply.github.com>
2024-06-02 18:16:44 +0200

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -18,6 +18,6 @@ def do_pack():
     local('mkdir -p versions')
     create = local('tar -cvzf versions/{} web_static'.format(archive))
     if create is not None:
-        return archive
+        return archives
     else:
         return None
