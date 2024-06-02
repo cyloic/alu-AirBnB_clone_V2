9acf7cb078a498402a5b3fa5d57de579f04e6040
cyloic <145006763+cyloic@users.noreply.github.com>
2024-06-02 18:18:36 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
+Fabric script based on the file 2-do_deploy_web_static.py that create and
 distributes an archive to the web servers
 
 execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
